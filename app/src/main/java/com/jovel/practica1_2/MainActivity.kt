10d7e0001afddbf2d34fc8ca639061264c41b562
JulianOcampoVelez <julian.ocampov@udea.edu.co>
2021-04-11 16:36:41 -0500

    Función de decimales y tamaño de spinner

--- app/src/main/java/com/jovel/practica1_2/MainActivity.kt
+++ app/src/main/java/com/jovel/practica1_2/MainActivity.kt
@@ -3,12 +3,13 @@ package com.jovel.practica1_2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jovel.practica1_2.databinding.ActivityMainBinding
+import java.text.DecimalFormat
 
 private const val EURO = "Euro (EUR)"
 private const val DOLAR = "Dólar Estadounidense (USD)"
 private const val PESOS = "Peso Colombiano (COP)"
 private const val preEURO = " EUR"
-private const val preDOLAR = "USD"
+private const val preDOLAR = " USD"
 private const val prePESOS = " COP"
 
 class MainActivity : AppCompatActivity() {
@@ -31,28 +32,28 @@ class MainActivity : AppCompatActivity() {
                     mainBinding.amountTextView.text = cantidad + prePESOS
                 }
                 if (divisa1 == PESOS && divisa2 == DOLAR) {
-                    mainBinding.amountTextView.text = (cantidad.toFloat() * 0.0003).toString() + preDOLAR
+                    mainBinding.amountTextView.text = (toFourDigits((cantidad.toFloat()*0.0003).toFloat())).toString() + preDOLAR
                 }
                 if (divisa1 == PESOS && divisa2 == EURO) {
-                    mainBinding.amountTextView.text = (cantidad.toFloat() * 0.0002).toString() + preEURO
+                    mainBinding.amountTextView.text = (toFourDigits((cantidad.toFloat()*0.0002).toFloat())).toString() + preEURO
                 }
                 if (divisa1 == DOLAR && divisa2 == DOLAR) {
                     mainBinding.amountTextView.text = cantidad + preDOLAR
                 }
                 if (divisa1 == DOLAR && divisa2 == PESOS) {
-                    mainBinding.amountTextView.text = (cantidad.toFloat() * 3554.19).toString() + prePESOS
+                    mainBinding.amountTextView.text = (toFourDigits((cantidad.toFloat()*3554.19).toFloat())).toString() + prePESOS
                 }
                 if (divisa1 == DOLAR && divisa2 == EURO) {
-                    mainBinding.amountTextView.text = (cantidad.toFloat() * 0.84).toString() + preEURO
+                    mainBinding.amountTextView.text = (toFourDigits((cantidad.toFloat()*0.84).toFloat())).toString() + preEURO
                 }
                 if (divisa1 == EURO && divisa2 == EURO) {
                     mainBinding.amountTextView.text = cantidad + preEURO
                 }
                 if (divisa1 == EURO && divisa2 == PESOS) {
-                    mainBinding.amountTextView.text = (cantidad.toFloat() * 4234.29).toString() + prePESOS
+                    mainBinding.amountTextView.text = (toFourDigits((cantidad.toFloat()*4234.29).toFloat())).toString() + prePESOS
                 }
                 if (divisa1 == EURO && divisa2 == DOLAR) {
-                    mainBinding.amountTextView.text = (cantidad.toFloat() * 1.19).toString() + preDOLAR
+                    mainBinding.amountTextView.text = (toFourDigits((cantidad.toFloat()*1.19).toFloat())).toString() + preDOLAR
                 }
             }
         }
