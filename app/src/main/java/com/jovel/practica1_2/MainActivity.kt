fa82f0843154de244dae8198d99a6a7fcffab7f8
JulianOcampoVelez <julian.ocampov@udea.edu.co>
2021-04-11 14:55:18 -0500

    Se definen constantes en los condicionales

--- app/src/main/java/com/jovel/practica1_2/MainActivity.kt
+++ app/src/main/java/com/jovel/practica1_2/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jovel.practica1_2.databinding.ActivityMainBinding
 
+private const val EURO = "Euro (EUR)"
+private const val DOLAR = "Dólar Estadounidense (USD)"
+private const val PESOS = "Peso Colombiano (COP)"
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
@@ -20,31 +24,31 @@ class MainActivity : AppCompatActivity() {
             val divisa2 = mainBinding.currencyList2.selectedItem.toString()
 
             if (cantidad.isNotEmpty()) {
-                if (divisa1 == "Peso Colombiano (COP)" && divisa2 == "Peso Colombiano (COP)") {
+                if (divisa1 == PESOS && divisa2 == PESOS) {
                     mainBinding.amountTextView.text = cantidad + "  COP"
                 }
-                if (divisa1 == "Peso Colombiano (COP)" && divisa2 == "Dólar Estadounidense (USD)") {
+                if (divisa1 == PESOS && divisa2 == DOLAR) {
                     mainBinding.amountTextView.text = (cantidad.toFloat() * 0.0003).toString() + "  USD"
                 }
-                if (divisa1 == "Peso Colombiano (COP)" && divisa2 == "Euro (EUR)") {
+                if (divisa1 == PESOS && divisa2 == EURO) {
                     mainBinding.amountTextView.text = (cantidad.toFloat() * 0.0002).toString() + "  EUR"
                 }
-                if (divisa1 == "Dólar Estadounidense (USD)" && divisa2 == "Dólar Estadounidense (USD)") {
+                if (divisa1 == DOLAR && divisa2 == DOLAR) {
                     mainBinding.amountTextView.text = cantidad + "  USD"
                 }
-                if (divisa1 == "Dólar Estadounidense (USD)" && divisa2 == "Peso Colombiano (COP)") {
+                if (divisa1 == DOLAR && divisa2 == PESOS) {
                     mainBinding.amountTextView.text = (cantidad.toFloat() * 3554.19).toString() + "  COP"
                 }
-                if (divisa1 == "Dólar Estadounidense (USD)" && divisa2 == "Euro (EUR)") {
+                if (divisa1 == DOLAR && divisa2 == EURO) {
                     mainBinding.amountTextView.text = (cantidad.toFloat() * 0.84).toString() + "  EUR"
                 }
-                if (divisa1 == "Euro (EUR)" && divisa2 == "Euro (EUR)") {
+                if (divisa1 == "Euro (EUR)" && divisa2 == EURO) {
                     mainBinding.amountTextView.text = cantidad + "  EUR"
                 }
-                if (divisa1 == "Euro (EUR)" && divisa2 == "Peso Colombiano (COP)") {
+                if (divisa1 == EURO && divisa2 == "Peso Colombiano (COP)") {
                     mainBinding.amountTextView.text = (cantidad.toFloat() * 4234.29).toString() + "  COP"
                 }
-                if (divisa1 == "Euro (EUR)" && divisa2 == "Dólar Estadounidense (USD)") {
+                if (divisa1 == EURO && divisa2 == "Dólar Estadounidense (USD)") {
                     mainBinding.amountTextView.text = (cantidad.toFloat() * 1.19).toString() + "  USD"
                 }
             }
